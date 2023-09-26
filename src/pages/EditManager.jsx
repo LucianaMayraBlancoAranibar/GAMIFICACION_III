3d5b6ffac0077faf68ddcac19b5591730faed2fe
Steven Gabriel Claros Tapia <88797832+StevenGCT@users.noreply.github.com>
2023-09-26 18:01:51 -0400

    Arreglando estilo css

--- src/pages/EditManager.jsx
+++ src/pages/EditManager.jsx
@@ -9,96 +9,99 @@ import Datepicker from '../components/Datepicker';
 function EditManager() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  };
 
   return (
-   
-   <div className="flex h-screen overflow-hidden">
 
-    {/* Sidebar */}
-    <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-    
-    <div className="relative p-4 sm:p-6 rounded-sm overflow-hidden mb-8 w-1/2">
-    <div className="relative">
-        <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">Editar Gestor </h1>
-      
+    <div className="flex h-screen overflow-hidden">
+
+      {/* Sidebar */}
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      {/* Content area */}
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        {/*  Site header */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="relative p-4 sm:p-6 rounded-sm overflow-hidden mb-8 w-1/2">
+          <div className="relative">
+            <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">Editar Gestor </h1>
+
+          </div>
+
+          <form className="relative p-4 sm:p-6 rounded-sm overflow-hidden mb-50">
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">Nombre:</label>
+              <input
+                type="text"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">
+                Apellido:
+              </label>
+              <input
+                type="text"
+
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">
+                Carrera:
+              </label>
+              <select
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="Carrera 1">Carrera 1</option>
+                <option value="Carrera 2">Carrera 2</option>
+                <option value="Carrera 3">Carrera 3</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">
+                Email:
+              </label>
+              <input
+                type="text"
+
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">
+                Contraseña:
+              </label>
+              <input
+                type="text"
+
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">
+                Rol:
+              </label>
+              <select
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              >
+                <option value="Carrera 1">Rol 1</option>
+                <option value="Carrera 2">Rol 2</option>
+                <option value="Carrera 3">Rol 3</option>
+              </select>
+            </div>
+            <button
+              type="submit"
+              className="px-10 py-5 leading-5 text-white transition-colors duration-200 transform bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-600"
+              >
+              Actualizar
+            </button>
+          </form>
+        </div>
+
       </div>
-      
-      <form className="relative p-4 sm:p-6 rounded-sm overflow-hidden mb-50">
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2">Nombre:</label>
-          <input
-            type="text"
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2">
-            Apellido:
-          </label>
-          <input
-            type="text"
-          
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2">
-            Carrera:
-          </label>
-          <select
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
-            <option value="Carrera 1">Carrera 1</option>
-            <option value="Carrera 2">Carrera 2</option>
-            <option value="Carrera 3">Carrera 3</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2">
-            Email:
-          </label>
-          <input
-            type="text"
-          
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2">
-            Contraseña:
-          </label>
-          <input
-            type="text"
-          
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2">
-            Rol:
-          </label>
-          <select
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          >
-            <option value="Carrera 1">Rol 1</option>
-            <option value="Carrera 2">Rol 2</option>
-            <option value="Carrera 3">Rol 3</option>
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
-        >
-          Actualizar
-        </button>
-      </form>
     </div>
-    
-    </div>
-  
   );
 }
 
