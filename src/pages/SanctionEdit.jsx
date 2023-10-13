69ca40be891544a9383f112e4f41c95fbec1fe8f
LucianaMayraBlancoAranibar <bal0029274@est.univalle.edu>
2023-10-13 01:36:31 -0400

    int 2

---
+++ src/pages/SanctionEdit.jsx
@@ -0,0 +1,240 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Sidebar from "../partials/Sidebar";
+import Header from "../partials/Header";
+
+import ModalConfirmacion from "../partials/ModalConfirmacion";
+
+function SanctionEdit() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { id } = useParams();
+  const [idStudent, setidStudent] = useState("");
+  const [sancion, setSancion] = useState({
+    description: "",
+    sanction1: "",
+    idStudent: "",
+    idProfessor: "",
+    idAdministrator: "",
+  });
+  const [Estudiante, setEstudiante] = useState("");
+
+  const [descriptionError, setDescriptionError] = useState("");
+  const [sanction1Error, setSanction1Error] = useState("");
+
+  const validateForm = () => {
+    let isValid = true;
+
+    if (!sancion.description) {
+      setDescriptionError("La descripción es obligatoria");
+      isValid = false;
+    } else if (sancion.description.length < 5 || sancion.description.length > 25) {
+      setDescriptionError("La descripción debe tener entre 3 y 25 caracteres");
+      isValid = false;
+    } else {
+      setDescriptionError("");
+    }
+
+    if (!sancion.sanction1) {
+      setSanction1Error("El puntaje es obligatorio");
+      isValid = false;
+    } else if (sancion.sanction1 == 0) {
+      setSanction1Error("El puntaje debe ser mayor a 0");
+      isValid = false;
+    } else {
+      setSanction1Error("");
+    }
+
+    return isValid;
+  };
+
+  useEffect(() => {
+    axios
+      .get(`https://localhost:7220/api/sanctions/${id}`)
+      .then((response) => {
+        setSancion(response.data);
+        setidStudent(response.data.idStudent);
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    
+      axios
+      .get("https://localhost:7220/api/Students")
+      .then((response) => {
+        console.log(response.data); // Verifica los datos que obtienes
+        setEstudiante(response.data);
+      })
+      .catch((error) => {
+        console.error(error); // Verifica si hay errores en la llamada a la API
+      });    
+  }, [id]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "idStudent") {
+      setidStudent(value); // Actualiza idStudent directamente
+    }
+    setSancion((prevSancion) => ({
+      ...prevSancion,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+
+    // Crear un objeto con los datos en formato JSON
+    const requestData = {
+      idSanctions: id,
+      description: sancion.description,
+      sanction1: sancion.sanction1,
+      idProfessor: sancion.idProfessor,
+      idAdministrator: sancion.idAdministrator,
+      idStudent: idStudent,
+    };
+
+    axios
+      .put(`https://localhost:7220/api/sanctions/${id}`, requestData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  };
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="relative p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
+          <div className="relative">
+            <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">
+              Editar sancion{" "}
+            </h1>
+          </div>
+          <br></br>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <div>
+                <label
+                  htmlFor="description"
+                  className="text-gray-900 dark:text-gray-900"
+                >
+                  Descripción
+                </label>
+                <input
+                  type="text"
+                  name="description"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={sancion.description}
+                  onChange={handleInputChange}
+                />
+                {descriptionError && (
+                  <p className="text-red-500">{descriptionError}</p>)}
+                <br />
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="sanction1"
+                >
+                  Puntaje:
+                </label>
+                <input
+                  type="number"
+                  name="sanction1"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={sancion.sanction1}
+                  onChange={handleInputChange}
+                />
+                {sanction1Error && (
+                  <p className="text-red-500">{sanction1Error}</p>)}
+                <br/>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="idStudent"
+                >
+                  Estudiante:
+                </label>
+                <br/>
+                {Estudiante.length === 0 ? (
+                  <p>Cargando datos...</p>
+                ) : (
+                  <select
+                    id="idStudent"
+                    className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                    value={idStudent}
+                    onChange={(e) => setIdStudent(e.target.value)}
+                  >
+                    {Estudiante.map((estudiante) => (
+                      <option
+                        key={estudiante.idStudent}
+                        value={estudiante.idStudent}
+                      >
+                        {estudiante.firstName + " " + estudiante.lastName}
+                      </option>
+                    ))}
+                  </select>
+                )}
+                <br/>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="idStudent"
+                >
+                  maestro:
+                </label>
+                <input
+                  type="number"
+                  name="idProfessor"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={sancion.idProfessor}
+                  onChange={(e) => setIdProfessor(e.target.value)}
+                />
+                <br/>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="idAdministrator"
+                >
+                  administrador:
+                </label>
+                <input
+                  type="number"
+                  name="idAdministrator"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={sancion.idAdministrator}
+                  onChange={(e) => setidAdministrator(e.target.value)}
+                />
+              </div>
+              <br></br>
+              <div className="flex justify-left">
+                <button
+                  className="px-10 py-5 leading-5 text-white transition-colors duration-200 transform bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-600"
+                  type="submit"
+                >
+                  Guardar Cambios
+                </button>
+
+              </div>
+              <br></br>
+              <br></br>
+              <Link to="/SanctionTable">Volver a la lista de sanciones</Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SanctionEdit;
---
+++ src/pages/SanctionForm.jsx
@@ -0,0 +1,235 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Sidebar from "../partials/Sidebar";
+import Header from "../partials/Header";
+import ModalConfirmacion from "../partials/ModalConfirmacion";
+import { Link } from "react-router-dom";
+
+function SanctionForm() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [description, setDescription] = useState("");
+  const [sanction1, setSanction1] = useState("");
+  const [idStudent, setIdStudent] = useState("");
+  const [idProfessor, setIdProfessor] = useState("");
+  const [idAdministrator, setidAdministrator] = useState("");
+  const [Estudiante, setEstudiante] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  //Validacion
+  const [descriptionError, setDescriptionError] = useState("");
+  const [sanction1Error, setSanction1Error] = useState("");
+  const [idStudentError, setIdStudentError] = useState("");
+
+  const validateForm = () => {
+    let isValid = true;
+
+    if (!description) {
+      setDescriptionError("La descripción es obligatoria");
+      isValid = false;
+    } else if (description.length < 5 || description.length > 25) {
+      setDescriptionError("La descripción debe tener entre 3 y 25 caracteres");
+      isValid = false;
+    } else {
+      setDescriptionError("");
+    }
+
+    if (!sanction1) {
+      setSanction1Error("El puntaje es obligatorio");
+      isValid = false;
+    } else if (sanction1 == 0) {
+      setSanction1Error("El puntaje debe ser mayor a 0");
+      isValid = false;
+    } else {
+      setSanction1Error("");
+    }
+
+    if (!idStudent) {
+      setIdStudentError("Debes seleccionar un estudiante");
+      isValid = false;
+    } else {
+      setIdStudentError("");
+    }
+
+    return isValid;
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://localhost:7220/api/Students")
+      .then((response) => {
+        console.log(response.data); // Verifica los datos que obtienes
+        setEstudiante(response.data);
+      })
+      .catch((error) => {
+        console.error(error); // Verifica si hay errores en la llamada a la API
+      });
+
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (validateForm()) {
+
+      const data = {
+        description: description,
+        sanction1: sanction1,
+        idStudent: idStudent,
+        idProfessor: idProfessor,
+        idAdministrator: idAdministrator,
+      };
+
+      try {
+        const response = await axios.post(
+          "https://localhost:7220/api/Sanctions",
+          data
+        );
+
+        console.log("Sancion registrada con éxito:", response.data);
+
+        setModalIsOpen(true);
+        description("");
+        sanction1("");
+        idStudent("");
+        idProfessor("");
+        idAdministrator("");
+      } catch (error) {
+        console.error("Error al registrar la Sancion:", error);
+      }
+    }
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="relative p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
+          <div className="relative">
+            <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">
+              Nueva Sanción{" "}
+            </h1>
+          </div>
+          <br></br>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <div>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="description"
+                >
+                  Descripción:
+                </label>
+                <input
+                  type="text"
+                  id="description"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+                {descriptionError && (
+                  <p className="text-red-500">{descriptionError}</p>)}
+                <br/>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="sanction1"
+                >
+                  Puntaje:
+                </label>
+                <input
+                  type="number"
+                  id="sanction1"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={sanction1}
+                  onChange={(e) => setSanction1(e.target.value)}
+                />
+                {sanction1Error && (
+                  <p className="text-red-500">{sanction1Error}</p>)}
+                <br/>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="idStudent"
+                >
+                  Estudiante:
+                </label>
+                <br/>
+                {Estudiante.length === 0 ? (
+                  <p>Cargando datos...</p>
+                ) : (
+                  <select
+                    id="idStudent"
+                    className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                    value={idStudent}
+                    onChange={(e) => {
+                      console.log("Valor seleccionado en el select:", e.target.value); // Agrega este console.log
+                      setIdStudent(e.target.value);
+                    }} 
+                    >
+                    <option value="">Selecciona un estudiante</option>
+                    {Estudiante.map((estudiante) => (
+                      <option
+                        key={estudiante.idStudent}
+                        value={estudiante.idStudent}
+                      >
+                        {estudiante.firstName + " " + estudiante.lastName}
+                      </option>
+                    ))}
+                  </select>
+                )}
+                {idStudentError && (
+                  <p className="text-red-500">{idStudentError}</p>
+                )}
+                <br/>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="idStudent"
+                >
+                  maestro:
+                </label>
+                <input
+                  type="number"
+                  id="idProfessor"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={idProfessor}
+                  onChange={(e) => setIdProfessor(e.target.value)}
+                />
+                <br/>
+                <label
+                  className="text-gray-900 dark:text-gray-900"
+                  htmlFor="idAdministrator"
+                >
+                  administrador:
+                </label>
+                <input
+                  type="number"
+                  id="idAdministrator"
+                  className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  value={idAdministrator}
+                  onChange={(e) => setidAdministrator(e.target.value)}
+                />
+              </div>
+              <br></br>
+              <div className="flex justify-left">
+                <button
+                  className="px-10 py-5 leading-5 text-white transition-colors duration-200 transform bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-600"
+                  type="submit"
+                >
+                  Registrar
+                </button>
+              </div>
+            </div>
+            <br></br>
+            <Link to="/SanctionTable">Volver a la lista de sanciones</Link>
+          </form>
+          {/* Modal de confirmación */}
+          <ModalConfirmacion isOpen={modalIsOpen} closeModal={closeModal} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SanctionForm;
